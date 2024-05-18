dc24ead6fdafa1c43a0262a99b22305bac31c9ee
Daeyun <sweet_ray@naver.com>
2024-05-18 22:18:02 +0900

    Feat:  초기 UI 구성 코드 구현

--- newlyCoinedWord/ViewController.swift
+++ newlyCoinedWord/ViewController.swift
@@ -25,12 +25,53 @@ class ViewController: UIViewController {
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureUI()
     }
-    
     
     //MARK: - Configurations
     
+    func configureUI() {
+        backView.backgroundColor = .white
+        backView.layer.borderColor = UIColor.black.cgColor
+        backView.layer.borderWidth = 2
+        
+        searchTextField.borderStyle = .none
+        searchTextField.tintColor = .black
+        searchTextField.text = "삼귀자"
+        
+        searchButton.backgroundColor = .black
+        searchButton.tintColor = .white
+        searchButton.setTitle("", for: .normal)
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .unspecified, scale: .default)
+        let symbolImage = UIImage(systemName: "magnifyingglass", withConfiguration: symbolConfig)
+        searchButton.setImage(symbolImage, for: .normal)
+        
+        firstAutoInputButton.setTitle("윰차", for: .normal)
+        configureAutoInputButton(button: firstAutoInputButton)
+        
+        secondAutoInputButton.setTitle("실매", for: .normal)
+        configureAutoInputButton(button: secondAutoInputButton)
+        
+        thirdAutoInputButton.setTitle("만만잘부", for: .normal)
+        configureAutoInputButton(button: thirdAutoInputButton)
+        
+        fourthAutoInputButton.setTitle("꾸안꾸", for: .normal)
+        configureAutoInputButton(button: fourthAutoInputButton)
+        
+        backImageView.image = UIImage.background
+        backImageView.contentMode = .scaleAspectFill
+        
+        searchResultLabel.text = "연애를 시작하기 전 썸 단계!"
+        searchResultLabel.textAlignment = .center
+        searchResultLabel.numberOfLines = 0
+    }
+    
+    func configureAutoInputButton(button: UIButton) {
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1
+        button.tintColor = .black
+        button.layer.cornerRadius = 12
+    }
     
     //MARK: - Functions
     
